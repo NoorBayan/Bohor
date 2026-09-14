1489c82af04742c0afff86a6083b6286749bcb25
NoorBayan <157401879+NoorBayan@users.noreply.github.com>
2026-09-14 03:37:02 +0300

    Create __init__.py

---
+++ bohor/preprocessing/__init__.py
@@ -0,0 +1,8 @@
+
+from bohor.preprocessing.normalizer import TextNormalizer
+from bohor.preprocessing.syllabifier import ProsodicSyllabifier
+
+__all__ = [
+    "TextNormalizer",
+    "ProsodicSyllabifier",
+]
